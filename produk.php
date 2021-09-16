fa549a63f18c289334f6470767ad29d722411c8c
Herlan Nurachman <58686408+Herlan21@users.noreply.github.com>
2021-09-16 16:38:45 +0700

    Add files via upload
    Menyelesaikan video ke 2

--- produk.php
+++ produk.php
@@ -1,9 +1,40 @@
 <?php
 class Produk{
+    public  $judul = "judul", 
+            $penulis = "penulis" , 
+            $penerbit = "penerbit",
+            $harga = 0;
 
+            public function getLabel(){
+                return "$this->penulis, $this->penerbit";
+            }
 }
-$produk1 = new Produk();
-$produk2 = new Produk();
 
-var_dump($produk1);
-var_dump($produk2);
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahProperty = "hahaha";
+// var_dump($produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = "30000";
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckmann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = "250000";
+
+echo "Komik : $produk3->penulis, $produk3->penerbit";
+echo "<br>";
+echo $produk3->getLabel();
+
+echo "komik : " . $produk3->getLabel();
+echo "<br>";
+echo "game : " . $produk3->getLabel();
