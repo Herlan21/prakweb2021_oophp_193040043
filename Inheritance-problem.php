72246628c2ab80d860dc8f076006e250c0567a63
herlan21 <herlan.n9d@gmail.com>
2021-09-23 14:51:06 +0700

    menyelesaikan video ke 1 inheritance bagian 1

---
+++ Inheritance-problem.php
@@ -0,0 +1,76 @@
+<?php
+
+use Produk as GlobalProduk;
+
+class Produk{
+    public  $judul,  
+            $penulis, 
+            $penerbit,  
+            $harga,
+            $jmlHalaman,
+            $waktuMain,
+            $tipe;
+
+            public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0, $tipe){
+                $this->judul = $judul;
+                $this->penulis = $penulis;
+                $this->penerbit = $penerbit;
+                $this->harga = $harga;
+                $this->waktuMain = $waktuMain;
+                $this->jmlHalaman = $jmlHalaman;
+                $this->tipe = $tipe;
+            }
+
+            public function getLabel(){
+                return "$this->penulis, $this->penerbit";
+            }
+
+            public function getInfoLengkap(){
+                $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} {Rp. {$this->harga})";
+                
+                if($this->tipe == "Komik") {
+                    $str .= "- {$this->jmlHalaman} Halaman.";
+                
+                } else if($this->tipe == "Game") {
+                    $str .= "- {$this->waktuMain} Jam.";
+                }
+                return $str;
+        
+            }
+}
+
+class CetakInfoProduk {
+    public function cetak( Produk $produk ) {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+            return $str;
+    }
+}
+
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0, "Komik");
+$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50, "Game");
+
+echo  $produk1->getInfoLengkap();
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// echo "Komik : " . $produk1->getLabel();
+// echo "<br>";
+// echo "Game : " . $produk2->getLabel();
+// echo "<br>";
+
+// $infoproduk1 = new CetakInfoProduk();
+// echo $infoproduk1->cetak($produk1);
